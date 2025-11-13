6864499474139e128e7a5d21545013661323e6c8
Sava Dragos Andrei <savaandreidragos2@gmail.com>
2025-11-13 14:50:46 +0200

    A, B1

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var09/PracticalTest01Var09MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var09/PracticalTest01Var09MainActivity.kt
@@ -1,16 +1,55 @@
 package ro.pub.cs.systems.eim.practicaltest01var09
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.StringTokenizer
+
 
 class PracticalTest01Var09MainActivity : AppCompatActivity() {
+
+    private var addButton: Button? = null
+    private var computeButton: Button? = null
+    private var nextTermEditText: EditText? = null
+    private var allTermsTextView: TextView? = null
+
+    private var sum: String? = null
+    private var number: String? = null
+    private var sumSum = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_practical_test01_var09_main)
 
+        addButton = findViewById(R.id.AddButton)
+        computeButton = findViewById(R.id.ComputeButton)
+        nextTermEditText = findViewById(R.id.NextTermEditText)
+        allTermsTextView = findViewById(R.id.AllTermsTextView)
+
+        addButton?.setOnClickListener {
+            number = nextTermEditText?.text.toString()
+            sum = allTermsTextView?.text.toString()
+            sum = if (sum!!.isEmpty()) {
+                number
+            } else {
+                "$sum + $number"
+            }
+            allTermsTextView?.text = sum
+            nextTermEditText?.setText("")
+        }
+
+        computeButton?.setOnClickListener {
+            sum = allTermsTextView?.text.toString()
+            sumSum = 0
+            sum?.split(" + ")?.forEach { token ->
+                sumSum += token.toInt()
+            }
+            Toast.makeText(this, sumSum.toString(), Toast.LENGTH_LONG).show()
+        }
     }
+
 }
+
